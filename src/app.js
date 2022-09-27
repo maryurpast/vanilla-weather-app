474a6fc4362e6394aeb731080a7aafb687733666
Maryna <maryurpast@gmail.com>
2022-09-27 17:54:31 +0300

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -35,6 +35,15 @@ function displayTemperature(response) {
   document.querySelector("#date").innerHTML = formatDate(
     response.data.dt * 1000
   );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "355e9d8397df6576cd27184097b1272d";
